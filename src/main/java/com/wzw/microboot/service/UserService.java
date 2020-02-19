623500c45d7d9426df8faa29caafe5cb3c5b9d0e
bugua123 <47909592@163.com>
2020-02-19 18:32:32 +0800

    修正service

---
+++ src/main/java/com/wzw/microboot/service/UserService.java
@@ -0,0 +1,16 @@
+package com.wzw.microboot.service;
+
+import com.wzw.microboot.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public interface UserService {
+    List<User> selectAll();
+    User getOne(String id);
+    void delete(String id);
+    void insert(User user);
+    void update(User user);
+    User checkUser(String userName, String passWord);
+
+}
