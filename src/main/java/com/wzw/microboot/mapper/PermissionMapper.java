7907e651d9baab18fd4715f340b1c4af97c3badf
bugua123 <47909592@163.com>
2020-02-28 21:55:58 +0800

    实现菜单管理

--- src/main/java/com/wzw/microboot/mapper/PermissionMapper.java
+++ src/main/java/com/wzw/microboot/mapper/PermissionMapper.java
@@ -2,6 +2,14 @@ package com.wzw.microboot.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wzw.microboot.entity.Permission;
+import org.apache.ibatis.annotations.Param;
+
+import java.io.Serializable;
 
 public interface PermissionMapper extends BaseMapper<Permission> {
+    /**
+     * 根据权限或菜单ID删除权限表和角色的关系表里面的数据
+     * @param id
+     */
+    void deleteRolePermissionByPid(@Param("id") Serializable id);
 }
--- src/main/java/com/wzw/microboot/service/impl/PermissionServiceImpl.java
+++ src/main/java/com/wzw/microboot/service/impl/PermissionServiceImpl.java
@@ -7,7 +7,17 @@ import com.wzw.microboot.service.PermissionService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.Serializable;
+
 @Service
 @Transactional
 public class PermissionServiceImpl extends ServiceImpl<PermissionMapper, Permission> implements PermissionService {
+    @Override
+    public boolean removeById(Serializable id) {
+
+        //根据权限或菜单ID删除权限表各和角色的关系表里面的数据
+        PermissionMapper permissionMapper = getBaseMapper();
+        permissionMapper.deleteRolePermissionByPid(id);
+        return super.removeById(id);
+    }
 }
